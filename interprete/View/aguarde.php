128f11be6e26f960d1707d2422b270eec159fe32
Amanda Pedreira <148297496+zksunn@users.noreply.github.com>
2025-08-14 20:56:57 -0300

    Create aguarde.php

---
+++ interprete/View/aguarde.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+        Em ate 48h sua conta sera avaliada. <br>
+        Fique de olho no seu email!
+    </center>
+
+    
+</body>
+</html>
