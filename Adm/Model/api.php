53804a058497e6c128e4108f0fc4b8f25af86a67
Amanda Pedreira <148297496+zksunn@users.noreply.github.com>
2025-08-14 21:05:25 -0300

    Create api.php

---
+++ Adm/Model/api.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'site_key' => '11983652-11be-48b8-b9d4-eaf88396f4d0',
+    'secret_key' => 'ES_07f5b8b6d544441491f9beaec59451a8',
+];
+
+class Api{
+    private $siteKeys = "11983652-11be-48b8-b9d4-eaf88396f4d0";
+    private $secret   = "ES_07f5b8b6d544441491f9beaec59451a8";
+}
+
+
+?>
